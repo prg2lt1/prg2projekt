17f001f6efb4d9dc564683e4bc792f64f35e14ab
Tobi <tobias.heer@stud.hslu.ch>
2015-05-12 12:10:50 +0200

    Bald geschafft...

--- src/GameModel/Board.java
+++ src/GameModel/Board.java
@@ -14,10 +14,9 @@ public class Board {
     
     private int size;
     
-    private static ArrayList<Box> boxList;
-    private static ArrayList<Line> horizontalLineList;
-    private static ArrayList<Line> verticalLineList;
-    private static ArrayList<Dot> dotList;
+    private ArrayList<Box> boxList;
+    private ArrayList<Line> lineList;
+    private ArrayList<Dot> dotList;
     
     /**
      * 
@@ -28,8 +27,7 @@ public class Board {
         this.size = size;
         
         dotList = createDots();
-        horizontalLineList = createHorizontalLines();
-        verticalLineList = createVerticalLines();
+        lineList = createLines();
         boxList = createBoxes();
         
     }
@@ -59,63 +57,59 @@ public class Board {
         return dotList;
     }
     
-    private ArrayList<Line> createHorizontalLines() {
+    private ArrayList<Line> createLines() {
         
-        horizontalLineList = new ArrayList<>();
+        lineList = new ArrayList<>();
         Dot first;
         Dot second;
         
-        for (int index=0; index <= dotList.size()-2; index++)  {
+        //Horizontal Lines
+        for (int index=0; index < dotList.size()-1; index++)  {
+            
+            if(index%size == size-1) {
+            index++;
+            }
+            
             first = dotList.get(index);
             second = dotList.get(index+1);
-            horizontalLineList.add(new Line(first, second));
+            lineList.add(new Line(first, second));
         }
-        return horizontalLineList;
-    }
-    
-    public ArrayList<Line> getHorizontalLines(){
-        return horizontalLineList;
-    }
-    
-    private ArrayList<Line> createVerticalLines() {
-        
-        verticalLineList = new ArrayList<>();
-        Dot first;
-        Dot second;
-        
-        for (int index=0; index <= dotList.size()-size-1; index++)  {
+        //Vertical Lines
+        for (int index=0; index < dotList.size()-size; index++)  {
             first = dotList.get(index);
-            second = dotList.get(index+4);
-            verticalLineList.add(new Line(first, second));
+            second = dotList.get(index+size);
+            lineList.add(new Line(first, second));
         }
-        return verticalLineList;
+        
+        
+        return lineList;
     }
-    
-    public ArrayList<Line> getVerticalLines(){
-        return verticalLineList;
-    } 
+   
     
      private ArrayList<Box> createBoxes() {
         
         boxList = new ArrayList<>();
+        int totalBoxes = (size-1)^2;
         Line top;
         Line bottom;
         Line left;
         Line right;
         
-        for (int index=0; horizontalLineList.size()-size >= index ; index++) {
-            
-        
-         int verticalIndex = 0;
-         top = horizontalLineList.get(index);
-         bottom = horizontalLineList.get(index+3);
+        for (int boxno = 0; boxno < totalBoxes; boxno++)    {
          
-         if(verticalIndex%size != size-1) {
+         int horizontalIndex = 0;   
+         int verticalIndex = size*(size-1);
+         top = lineList.get(horizontalIndex);
+         bottom = lineList.get(horizontalIndex+size-1);
+         
+         if(verticalIndex%size == size-1) {
              verticalIndex++;
          }
-         left = verticalLineList.get(verticalIndex);
-         right = verticalLineList.get(verticalIndex+1);
-         boxList.add(new Box(index, top, bottom, left, right ));
+         left = lineList.get(verticalIndex);
+         right = lineList.get(verticalIndex+1);
+         boxList.add(new Box(boxno, top, bottom, left, right ));
+         
+         horizontalIndex++;
          verticalIndex++;
          
         }
@@ -125,39 +119,11 @@ public class Board {
      public ArrayList<Box> getBoxes(){
          return boxList;
      }
-    
-  /**
      
-    public static void main(final String[] args) {
-        
-        
-        Board t = new Board(4);
-       for(int i = 0; i<dotList.size(); i++) {
-           System.out.println(dotList.get(i).getX());
-           System.out.println(dotList.get(i).getY());
-           System.out.println();
-       }
-           
-       for(int j = 0; j<horizontalLineList.size(); j++) {
-           System.out.println(horizontalLineList.get(j).getStartingDot().getX());
-           System.out.println(horizontalLineList.get(j).getStartingDot().getY());
-           System.out.println(horizontalLineList.get(j).getEndingDot().getX());
-           System.out.println(horizontalLineList.get(j).getEndingDot().getY());
-           System.out.println();
-       }
-           
-        for(int k = 0; k<verticalLineList.size(); k++) {
-           System.out.println(verticalLineList.get(k).getStartingDot().getX());
-           System.out.println(verticalLineList.get(k).getStartingDot().getY());
-           System.out.println(verticalLineList.get(k).getEndingDot().getX());
-           System.out.println(verticalLineList.get(k).getEndingDot().getY());
-           System.out.println();   
-           System.out.println();
-           System.out.println();
-           System.out.println();
-        }
-           System.out.println(boxList.get(4).getRightLine());
+    public ArrayList<Line> getLines(){
+        return lineList;
     }
-    */
+    
+ 
     
 }
