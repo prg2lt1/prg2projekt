1f4765a6f25e1940a02a5523fba6684cc72df01e
yokainsane <panhahe@hotmail.com>
2015-05-17 11:29:16 +0200

    Player and Opponent.. User is a Player-Instance?

---
+++ src/GameModel/Opponent.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameModel;
+
+/**
+ *
+ * @author Lorenz
+ */
+public class Opponent extends Player {
+
+    private String ipNmb;
+
+    public Opponent() {
+        super();
+    }
+
+    public Opponent(String newName, String newIp) {
+        super(newName);
+        this.ipNmb = newIp;
+    }
+
+    public void setIp(String newIp) {
+        this.ipNmb = newIp;
+    }
+
+    public String getIp() {
+        return this.ipNmb;
+    }
+}
--- src/GameModel/Player.java
+++ src/GameModel/Player.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package GameModel;
 
 /**
@@ -10,5 +5,36 @@ package GameModel;
  * @author tobias
  */
 public class Player {
+
+    private String name;
+    private int nmbOfBoxes;
+
+    public Player() {
+        this.nmbOfBoxes = 0;
+    }
+
+    public Player(String newName) {
+        this.name = newName;
+        this.nmbOfBoxes = 0;
+    }
+
+    public void plusBox() {
+        this.nmbOfBoxes++;
+    }
+
+    public void setNmbOfBoxesZero() {
+        this.nmbOfBoxes = 0;
+    }
+
+    public int getNmbOfBoxes() {
+        return this.nmbOfBoxes;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
     
+    public String getName()    {
+        return this.name;
+    }
 }
